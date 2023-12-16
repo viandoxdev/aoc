42f8be3782f0b30890e36cdbfa43803696991f4c
viandoxdev <titouangoulois29@gmail.com>
2023-12-16 13:17:14 +0100

    commit before day12 opt (500ms)

--- 2023/src/challenges/day13.rs
+++ 2023/src/challenges/day13.rs
@@ -1,5 +1,142 @@
 use anyhow::Result;
+use itertools::Itertools;
 
-pub async fn day13(_input: String) -> Result<(String, String)> {
-    todo!();
+// Vector of bits
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Bits(u32);
+
+#[derive(Debug)]
+enum Reflection {
+    Horizontal(usize),
+    Vertical(usize),
+}
+
+impl Reflection {
+    pub fn summarize(self) -> usize {
+        match self {
+            Self::Horizontal(x) => 100 * x,
+            Self::Vertical(x) => x,
+        }
+    }
+}
+
+/// Count number of ones in the binary representation of n
+const fn count_ones(mut n: usize) -> usize {
+    let mut count = 0;
+    while n > 0 {
+        count += 1;
+        n = n & (n - 1);
+    }
+    count
+}
+
+/// Number of ones in the binary representations of a byte
+static ONE_COUNT_MAP: [usize; 256] = {
+    let mut res = [0; 256];
+    let mut n = 0;
+    while n < 256 {
+        res[n] = count_ones(n);
+        n += 1;
+    }
+    res
+};
+
+impl Bits {
+    /// Number of bits with value one
+    fn ones(&self) -> usize {
+        // Count in each bytes and sum them up
+        let x0 = (self.0 & 0xFF) as usize;
+        let x1 = (self.0 >> 8 & 0xFF) as usize;
+        let x2 = (self.0 >> 16 & 0xFF) as usize;
+        let x3 = (self.0 >> 24) as usize;
+        ONE_COUNT_MAP[x0] + ONE_COUNT_MAP[x1] + ONE_COUNT_MAP[x2] + ONE_COUNT_MAP[x3]
+    }
+}
+
+impl std::ops::BitXor for Bits {
+    type Output = Self;
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        Self(self.0 ^ rhs.0)
+    }
+}
+
+/// Look for reflection in slice, expected_diff specifies how many bits of difference are expected.
+/// For a perfect reflection we expect 0, for a reflection with a single smudge we expect 1.
+fn find_reflection(slice: &[Bits], expected_diff: usize) -> Option<usize> {
+    let len = slice.len();
+    let mid = len / 2;
+    // Iterate from the middle and going left then right, ...
+    // (this is because most reflections seemed to be near the middle)
+    'outer: for i in (0..mid).rev().interleave(mid..(len - 1)) {
+        // How far we look for the reflection, because we don't need to do the whole slice if the
+        // reflection is off center
+        let reach = (i + 1).min(len - i - 1);
+        // Total difference from what we would expect of a reflection
+        let mut total_diff = 0;
+
+        for r in 0..reach {
+            // How many bits are different
+            let diff = (slice[i - r] ^ slice[i + 1 + r]).ones();
+
+            total_diff += diff;
+
+            if total_diff > expected_diff {
+                continue 'outer;
+            }
+        }
+
+        if total_diff == expected_diff {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+pub async fn day13(input: String) -> Result<(String, String)> {
+    // Parse the input into cols and rows, each containing bitvecs reprensenting the row / column
+    let parsed = input
+        .split("\n\n")
+        .map(|s| {
+            let width = s.find('\n').unwrap();
+
+            let rows = s
+                .lines()
+                .map(|l| Bits(l.chars().fold(0, |a, x| (a << 1) | (x == '#') as u32)))
+                .collect_vec();
+            let cols = (0..width)
+                .map(|i| {
+                    Bits(
+                        s[i..]
+                            .chars()
+                            .step_by(width + 1)
+                            .fold(0, |a, x| (a << 1) | (x == '#') as u32),
+                    )
+                })
+                .collect_vec();
+            (rows, cols)
+        })
+        .collect_vec();
+
+    // Part1 and Part2 are the same with expected_diff different
+    let (part1, part2) = (0..=1)
+        .map(|ediff| {
+            parsed
+                .iter()
+                .map(|(rows, cols)| {
+                    let horizontal =
+                        find_reflection(rows, ediff).map(|x| Reflection::Horizontal(x + 1));
+                    let vertical =
+                        find_reflection(cols, ediff).map(|x| Reflection::Vertical(x + 1));
+
+                    horizontal
+                        .xor(vertical)
+                        .expect("No reflection or both reflections")
+                })
+                .map(Reflection::summarize)
+                .sum::<usize>()
+        })
+        .collect_tuple().unwrap();
+
+    Ok((part1.to_string(), part2.to_string()))
 }
--- 2023/src/challenges/day16.rs
+++ 2023/src/challenges/day16.rs
@@ -1,5 +1,147 @@
 use anyhow::Result;
+use itertools::Itertools;
 
-pub async fn day16(_input: String) -> Result<(String, String)> {
-    todo!();
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
+use Alignment::*;
+use Direction::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Alignment {
+    Vertical,
+    Horizontal,
+    Both,
+}
+
+impl Alignment {
+    fn add_to(&self, into: &mut Option<Alignment>) {
+        if let Some(prev) = into {
+            if prev != self {
+                *prev = Both;
+            }
+        } else {
+            *into = Some(*self);
+        }
+    }
+
+    fn is_in(self, inside: Option<Alignment>) -> bool {
+        if let Some(al) = inside {
+            al == Both || al == self
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+impl Direction {
+    fn step(&self, x: usize, y: usize, width: usize, height: usize) -> Option<(usize, usize)> {
+        match self {
+            Up => Some((x, y.checked_sub(1)?)),
+            Left => Some((x.checked_sub(1)?, y)),
+            Right => (x < width - 1).then_some((x + 1, y)),
+            Down => (y < height - 1).then_some((x, y + 1)),
+        }
+    }
+
+    fn reflect(&self) -> Self {
+        match self {
+            Up => Right,
+            Left => Down,
+            Right => Up,
+            Down => Left,
+        }
+    }
+
+    fn reflect_reverse(&self) -> Self {
+        match self {
+            Up => Left,
+            Left => Up,
+            Right => Down,
+            Down => Right,
+        }
+    }
+
+    fn alignment(&self) -> Alignment {
+        match self {
+            Up | Down => Vertical,
+            Left | Right => Horizontal,
+        }
+    }
+}
+
+pub async fn day16(input: String) -> Result<(String, String)> {
+    let chars = input.chars().collect_vec();
+    let width = input.find('\n').unwrap();
+    let height = input.lines().count();
+
+    let solve = |start: (usize, usize, Direction)| {
+        let mut alignments = vec![None; chars.len()];
+        let mut next_alignments = alignments.clone();
+
+        let mut beams = vec![start];
+        let mut next_beams = Vec::new();
+        while !beams.is_empty() {
+            'beams: for (mut x, mut y, mut dir) in beams.drain(..) {
+                loop {
+                    let index = y * (width + 1) + x;
+
+                    let char = chars[index];
+                    if dir.alignment().is_in(alignments[index]) && char != '/' && char != '\\' {
+                        continue 'beams;
+                    }
+
+                    dir.alignment().add_to(&mut next_alignments[index]);
+
+                    match (char, dir.alignment()) {
+                        ('-', Vertical) => {
+                            next_beams.push((x, y, Left));
+                            next_beams.push((x, y, Right));
+                            continue 'beams;
+                        }
+                        ('|', Horizontal) => {
+                            next_beams.push((x, y, Up));
+                            next_beams.push((x, y, Down));
+                            continue 'beams;
+                        }
+                        _ => {
+                            dir = match char {
+                                '/' => dir.reflect(),
+                                '\\' => dir.reflect_reverse(),
+                                _ => dir,
+                            };
+
+                            let Some(next) = dir.step(x, y, width, height) else {
+                                continue 'beams;
+                            };
+
+                            (x, y) = next;
+                        }
+                    }
+                }
+            }
+
+            std::mem::swap(&mut beams, &mut next_beams);
+            alignments.copy_from_slice(&next_alignments[..]);
+        }
+
+        alignments.into_iter().flatten().count()
+    };
+
+    let starts = (0..height)
+        .flat_map(|y| [(0, y, Right), (width - 1, y, Left)])
+        .chain((0..width).flat_map(|x| [(x, 0, Down), (x, height - 1, Up)]))
+        .collect_vec();
+    let results = starts.par_iter().copied().map(solve).collect::<Vec<_>>();
+
+    let part1 = results[0];
+    let part2 = results.into_iter().max().unwrap();
+
+    Ok((part1.to_string(), part2.to_string()))
 }
