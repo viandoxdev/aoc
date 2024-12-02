ceba4f6419e6c126ce29d4e8b9c15fbc5372e435
viandoxdev <titouangoulois29@gmail.com>
2024-12-02 20:44:33 +0100

    be less dumb

--- 2024/days/Day02.ml
+++ 2024/days/Day02.ml
@@ -8,24 +8,21 @@ let parse_input =
 let parse input =
   Result.get_ok @@ Angstrom.parse_string ~consume: All parse_input input
 
-let report_error report =
-  let deltas = map (fun (p, c) -> p - c) @@ sliding_pairs report in
-  filter_map (fun (i, a) -> if a > 3 then Some i else None) @@ mapi (fun i d -> (i, abs d)) deltas,
-  filter_map (fun (i, a, b) -> if a != b || a == 0 || b == 0 then Some i else None) @@ mapi (fun i (a, b) -> (i, Int.compare a 0, Int.compare b 0)) @@ sliding_pairs deltas
-
 let check_report report =
-  let (deltas, monotony) = report_error report in
-  deltas = [] && monotony = []
+  let deltas = map (fun (p, c) -> p - c) @@ sliding_pairs report in
+  for_all (fun d -> d != 0 && abs d <= 3) deltas
+    && for_all (fun (a, b) -> a = b) 
+      @@ map (fun (a, b) -> (Int.compare a 0, Int.compare b 0)) @@ sliding_pairs deltas
 
 let check_report_dampened report =
-  let reports = report :: List.mapi (fun i _ -> remove_nth i report) report in
+  let reports = report :: mapi (fun i _ -> remove_nth i report) report in
   exists check_report reports
 
 let part1 reports =
-  List.length @@ List.filter check_report reports
+  length @@ filter check_report reports
 
 let part2 reports =
-  List.length @@ List.rev @@ List.filter check_report_dampened reports
+  length @@ filter check_report_dampened reports
 
 let day02 input =
   let reports = parse input in
