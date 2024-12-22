bebd1e924144ecee652b4a4c26fc4562ea4d1799
viandoxdev <titouangoulois29@gmail.com>
2024-12-22 12:19:54 +0100

    day 21

--- 2024/days/Day21.ml
+++ 2024/days/Day21.ml
@@ -1,23 +1,7 @@
 open Utils
 
-type key =
-  | N0
-  | N1
-  | N2
-  | N3
-  | N4
-  | N5
-  | N6
-  | N7
-  | N8
-  | N9
-  | A
-  | U
-  | L
-  | D
-  | R
-
-type keypad = { coords: (int * int) option array; path_to: int * int -> int * int -> (int * key) Iter.t }
+type key = N0 | N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9 | A | U | L | D | R
+type keypad = { coords : (int * int) option array; gap : int * int }
 
 let key_of_char = function
   | '0' -> N0
@@ -32,23 +16,6 @@ let key_of_char = function
   | '9' -> N9
   | 'A' -> A
   | _ -> failwith "No such key"
-
-let char_of_key = function
-  | N0 -> '0'
-  | N1 -> '1'
-  | N2 -> '2'
-  | N3 -> '3'
-  | N4 -> '4'
-  | N5 -> '5'
-  | N6 -> '6'
-  | N7 -> '7'
-  | N8 -> '8'
-  | N9 -> '9'
-  | A -> 'A'
-  | U -> '^'
-  | L -> '<'
-  | R -> '>'
-  | D -> 'v'
 
 let int_of_key = function
   | N0 -> 0
@@ -71,7 +38,7 @@ let dist (ax, ay) (bx, by) = abs (ax - bx) + abs (ay - by)
 
 let parse input =
   String.split_on_char '\n' input
-  |> List.filter ((<>) "")
+  |> List.filter (( <> ) "")
   |> List.map
        Iter.(
          fun s ->
@@ -83,100 +50,78 @@ let keypad_coords l =
   List.iter (fun (k, x, y) -> a.(int_of_key k) <- Some (x, y)) l;
   a
 
-let path_to_numeric (sx, sy) (ex, ey) =
+let make_memo = Hashtbl.create 2048
+
+let rec path ~memo f t =
   let open Iter in
-  let (gx, gy) = (0, 3) in
-  let dx, dy = (ex - sx, ey - sy) in
-  let hor = if dx < 0 then (abs dx, L) else (dx, R) in
-  let ver = if dy < 0 then (abs dy, U) else (dy, D) in
+  function
+  | [] -> 1
+  | pad :: pads -> (
+      let k = List.length pads in
+      match Hashtbl.find_opt memo (k, f, t) with
+      | Some r -> r
+      | None ->
+          let gx, gy = pad.gap in
+          let sx, sy = Option.get @@ pad.coords.(int_of_key f) in
+          let ex, ey = Option.get @@ pad.coords.(int_of_key t) in
+          let dx, dy = (ex - sx, ey - sy) in
+          let hor = repeat (if dx > 0 then R else L) |> take (abs dx) in
+          let ver = repeat (if dy > 0 then D else U) |> take (abs dy) in
+          let presses =
+            if dx = 0 then singleton (snoc ver A)
+            else if dy = 0 then singleton (snoc hor A)
+            else if sy = gy && ex = gx then singleton (snoc (append ver hor) A)
+            else if sx = gx && ey = gy then singleton (snoc (append hor ver) A)
+            else doubleton (snoc (append ver hor) A) (snoc (append hor ver) A)
+          in
 
-  if dy = 0 then
-    singleton hor
-  else if dx = 0 then
-    singleton ver
-  else if dx > 0 then
-    doubleton hor ver
-  else if dy < 0 && not (gx = sx && gy = ey) then
-    doubleton ver hor
-  else
-    doubleton ver hor
+          let res = presses |> map (path_through ~memo pads) |> min_exn in
 
-let path_to_directional (sx, sy) (ex, ey) =
+          Hashtbl.add memo (k, f, t) res;
+
+          res)
+
+and path_through ~memo p i =
   let open Iter in
-  let (gx, gy) = (0, 0) in
-  let dx, dy = (ex - sx, ey - sy) in
-  let hor = if dx < 0 then (abs dx, L) else (dx, R) in
-  let ver = if dy < 0 then (abs dy, U) else (dy, D) in
-
-  if dy = 0 then
-    singleton hor
-  else if dx = 0 then
-    singleton ver
-  else if dx > 0 then
-    doubleton hor ver
-  else if dy < 0 && not (gx = sx && gy = ey) then
-    doubleton ver hor
-  else
-    doubleton ver hor
+  i |> fold_map (fun prev cur -> (cur, path ~memo prev cur p)) A |> sum
 
 let numeric_pad =
-  { coords= keypad_coords
-    [
-      (N7, 0, 0);
-      (N8, 1, 0);
-      (N9, 2, 0);
-      (N4, 0, 1);
-      (N5, 1, 1);
-      (N6, 2, 1);
-      (N1, 0, 2);
-      (N2, 1, 2);
-      (N3, 2, 2);
-      (N0, 1, 3);
-      (A, 2, 3);
-    ];
-    path_to= path_to_numeric
+  {
+    coords =
+      keypad_coords
+        [
+          (N7, 0, 0);
+          (N8, 1, 0);
+          (N9, 2, 0);
+          (N4, 0, 1);
+          (N5, 1, 1);
+          (N6, 2, 1);
+          (N1, 0, 2);
+          (N2, 1, 2);
+          (N3, 2, 2);
+          (N0, 1, 3);
+          (A, 2, 3);
+        ];
+    gap = (0, 3);
   }
 
 let directional_pad =
-  { coords = keypad_coords
-    [ (U, 1, 0); (A, 2, 0); (L, 0, 1); (D, 1, 1); (R, 2, 1) ];
-    path_to = path_to_directional
+  {
+    coords =
+      keypad_coords [ (U, 1, 0); (A, 2, 0); (L, 0, 1); (D, 1, 1); (R, 2, 1) ];
+    gap = (0, 0);
   }
 
-let pad_code code pad =
+let solve pads codes =
   let open Iter in
-  code
-  |> fold_map
-       (fun p (m, k) ->
-         let n = Option.get @@ pad.coords.(int_of_key k) in
-         (n, append (pad.path_to p n) @@ singleton (m, A)))
-       (Option.get @@ pad.coords.(int_of_key A))
-  |> flatten
-
-let type_code pads code =
-  let open Iter in
-  List.fold_left pad_code (of_list code |> map (fun k -> (1, k))) pads
-    |> map (fun (n, _) -> n)
-    |> sum
-
-let string_of_code l =
-  Iter.(of_list l |> flat_map (fun (n, k) -> repeat (char_of_key k) |> take n) |> to_str) ^ " " ^ string_of_int Iter.(of_list l |> map (fun (n, _) -> n) |> sum)
-
-let type_code pads code =
-  let open Iter in
-  List.fold_left (fun c p -> print_endline @@ string_of_code c; to_list (pad_code (of_list c) p)) (on_list (map (fun k -> (1, k))) code) pads
-    |> (fun c -> print_endline @@ string_of_code c; c)
-    |> of_list
-    |> map (fun (n, _) -> n)
-    |> sum
-
-let part1 codes =
-  let open Iter in
-  let pads = [numeric_pad; directional_pad; directional_pad] in
+  let memo = make_memo in
   of_list codes
-    |> map (fun (code, n) -> n * type_code pads code)
-    |> sum
+  |> map (fun (keys, n) -> n * path_through ~memo pads (of_list keys))
+  |> sum
+
+let part1 = [ numeric_pad; directional_pad; directional_pad ]
+let part2 = numeric_pad :: (Array.to_list @@ Array.make 25 directional_pad)
 
 let day21 input =
   let codes = parse input in
-  (string_of_int @@ part1 codes, "TODO")
+  (string_of_int @@ solve part1 codes, string_of_int @@ solve part2 codes)
