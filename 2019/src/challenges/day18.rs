0b08a757901fb8f7d5fc71fd98330a7f5e862c76
viandoxdev <titouangoulois29@gmail.com>
2025-11-23 00:29:22 +0100

    commit before opt

--- 2019/src/challenges/day18.rs
+++ 2019/src/challenges/day18.rs
@@ -1,12 +1,104 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{BinaryHeap, VecDeque},
     hash::Hash,
+    ops::{Bound, RangeBounds},
     str::FromStr,
 };
 
 use anyhow::{Result, anyhow};
-use fixedbitset::FixedBitSet;
-use rustc_hash::FxHashSet;
+use itertools::Itertools;
+use rustc_hash::FxHashMap;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct BitSet {
+    inner: u32,
+}
+
+impl BitSet {
+    fn from_range(range: impl RangeBounds<usize>) -> Self {
+        fn to_included(b: Bound<&usize>, start: bool) -> usize {
+            match (start, b) {
+                (_, Bound::Included(&b)) => b,
+                (true, Bound::Excluded(&b)) => b + 1,
+                (false, Bound::Excluded(&b)) => b - 1,
+                (true, Bound::Unbounded) => 0,
+                (false, Bound::Unbounded) => 32,
+            }
+        }
+        let (start, end) = (
+            to_included(range.start_bound(), true),
+            to_included(range.end_bound(), false),
+        );
+        let len = end - start + 1;
+        Self {
+            inner: (1u32 << len).wrapping_sub(1) >> start,
+        }
+    }
+    #[inline(always)]
+    fn intersection(&self, other: &BitSet) -> Self {
+        Self {
+            inner: self.inner & other.inner,
+        }
+    }
+    #[inline(always)]
+    fn difference(&self, other: &BitSet) -> Self {
+        Self {
+            inner: self.inner & (!other.inner)
+        }
+    }
+    #[inline(always)]
+    fn empty() -> Self {
+        Self { inner: 0 }
+    }
+    #[inline(always)]
+    fn contains(&self, key: usize) -> bool {
+        self.inner & (1 << key) > 0
+    }
+    #[inline(always)]
+    fn insert(&mut self, key: usize) {
+        self.inner |= 1 << key;
+    }
+    #[inline(always)]
+    fn contains_set(&self, other: &BitSet) -> bool {
+        self.inner & other.inner == other.inner
+    }
+    #[inline(always)]
+    fn concat(&mut self, other: BitSet) {
+        self.inner |= other.inner
+    }
+}
+
+impl FromIterator<usize> for BitSet {
+    fn from_iter<T: IntoIterator<Item = usize>>(iter: T) -> Self {
+        let mut res = Self::empty();
+        for i in iter {
+            res.insert(i);
+        }
+        res
+    }
+}
+
+impl IntoIterator for BitSet {
+    type Item = usize;
+    type IntoIter = OnesIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        OnesIterator { set: self.inner }
+    }
+}
+
+struct OnesIterator {
+    set: u32,
+}
+
+impl Iterator for OnesIterator {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.set.trailing_zeros();
+        self.set ^= 1 << res;
+        (res != 32).then_some(res as usize)
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 enum Tile {
@@ -16,6 +108,7 @@ enum Tile {
     Key(u8),
 }
 
+#[derive(Clone)]
 struct Grid {
     width: usize,
     height: usize,
@@ -25,11 +118,7 @@ struct Grid {
 }
 
 impl Grid {
-    fn neighbors(
-        &self,
-        (x, y): (usize, usize),
-        grabbed_keys: &FixedBitSet,
-    ) -> impl Iterator<Item = ((usize, usize), Tile)> {
+    fn neighbors(&self, (x, y): (usize, usize)) -> impl Iterator<Item = ((usize, usize), Tile)> {
         [
             (x > 0).then_some((x.wrapping_sub(1), y)),
             (x < self.width - 1).then_some((x.wrapping_add(1), y)),
@@ -39,11 +128,28 @@ impl Grid {
         .into_iter()
         .flatten()
         .map(|(x, y)| ((x, y), self.tiles[y * self.width + x]))
-        .filter(|(_, tile)| match tile {
-            Tile::Wall => false,
-            Tile::Door(k) if !grabbed_keys.contains(*k as usize) => false,
-            _ => true,
-        })
+        .filter(move |(_, tile)| !matches!(tile, Tile::Wall))
+    }
+
+    fn transform(&self) -> Self {
+        let mut new = self.clone();
+        let (x, y) = new.entrance;
+        new.tiles[x + y * new.width] = Tile::Wall;
+        new.tiles[(x + 1) + y * new.width] = Tile::Wall;
+        new.tiles[(x - 1) + y * new.width] = Tile::Wall;
+        new.tiles[x + (y - 1) * new.width] = Tile::Wall;
+        new.tiles[x + (y + 1) * new.width] = Tile::Wall;
+        new
+    }
+
+    fn split_entrances(&self) -> [(usize, usize); 4] {
+        let (x, y) = self.entrance;
+        [
+            (x + 1, y + 1),
+            (x - 1, y + 1),
+            (x + 1, y - 1),
+            (x - 1, y - 1),
+        ]
     }
 }
 
@@ -95,88 +201,239 @@ impl FromStr for Grid {
     }
 }
 
-#[derive(Debug, Clone, Eq)]
-struct State {
-    pos: (usize, usize),
-    grabbed_keys: FixedBitSet,
-    steps: usize,
+#[derive(Debug, Clone, Copy, Eq)]
+struct State<const R: usize> {
+    poses: [usize; R],
+    keys: BitSet,
+    dist: usize,
 }
 
-impl PartialEq for State {
+impl<const R: usize> PartialEq for State<R> {
     fn eq(&self, other: &Self) -> bool {
-        self.pos == other.pos && self.grabbed_keys == other.grabbed_keys
+        self.poses == other.poses && self.keys == other.keys
     }
 }
 
-impl Hash for State {
+impl<const R: usize> Hash for State<R> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.pos.hash(state);
-        self.grabbed_keys.hash(state);
+        self.poses.hash(state);
+        self.keys.hash(state);
     }
 }
 
-impl State {
-    fn new(grid: &Grid) -> Self {
-        Self {
-            pos: grid.entrance,
-            grabbed_keys: FixedBitSet::with_capacity(grid.num_keys),
-            steps: 0,
-        }
+impl<const R: usize> Ord for State<R> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // Flip the order because a shorter distance is better than a longer one
+        other.dist.cmp(&self.dist)
     }
+}
 
-    fn step(&self, new_pos: (usize, usize), tile: Tile) -> Self {
-        let mut grabbed_keys = self.grabbed_keys.clone();
-        match tile {
-            Tile::Wall => panic!("Went into wall"),
-            Tile::Door(k) if !self.grabbed_keys.contains(k as usize) => {
-                panic!("Went through locked door")
+impl<const R: usize> PartialOrd for State<R> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+struct KeyGraph<const R: usize> {
+    robot_keys: [BitSet; R],
+    distances: Vec<Vec<usize>>,
+    dependencies: Vec<BitSet>,
+    count: usize,
+}
+
+impl<const R: usize> KeyGraph<R> {
+    fn new(grid: &Grid, robots: [(usize, usize); R]) -> Self {
+        // Index grid.num_keys is the distance from the robot's entrances.
+        let mut keys_distances = vec![vec![usize::MAX; grid.num_keys + 1]; grid.num_keys + 1];
+
+        let mut keys_robot = vec![0; grid.num_keys];
+        let mut keys_locations = vec![(0, 0); grid.num_keys];
+        let mut gate_requirements = vec![None; grid.num_keys];
+        let mut keys_requirements = vec![None; grid.num_keys];
+
+        let mut queue = VecDeque::new();
+        let mut visited = vec![false; grid.width * grid.height];
+
+        // Scan map from each entrances for key to entrances distances and what door needs to be
+        // unlocked to go where
+        for (robot, entrance) in robots.into_iter().enumerate() {
+            queue.push_back((entrance, None, 0));
+            visited[entrance.0 + entrance.1 * grid.width] = true;
+
+            while let Some((tile, req, dist)) = queue.pop_front() {
+                for ((x, y), tile) in grid.neighbors(tile) {
+                    if visited[x + grid.width * y] {
+                        continue;
+                    }
+
+                    if let Tile::Key(k) = tile {
+                        keys_requirements[k as usize] = req;
+                        keys_locations[k as usize] = (x, y);
+                        keys_robot[k as usize] = robot;
+                        keys_distances[k as usize][k as usize] = 0;
+                        keys_distances[k as usize][grid.num_keys] = dist + 1;
+                        keys_distances[grid.num_keys][k as usize] = dist + 1;
+                    }
+
+                    let next_req = if let Tile::Door(k) = tile {
+                        gate_requirements[k as usize] = req;
+                        Some(k)
+                    } else {
+                        req
+                    };
+                    queue.push_back(((x, y), next_req, dist + 1));
+                    visited[x + grid.width * y] = true;
+                }
             }
-            Tile::Key(k) => grabbed_keys.insert(k as usize),
-            _ => {}
-        };
+        }
+
+        // Do a few more bfs to firgure out the distances between each keys
+        let mut queue = VecDeque::new();
+        for k in 0..grid.num_keys {
+            visited.fill(false);
+            let (kx, ky) = keys_locations[k];
+            queue.push_back(((kx, ky), 0));
+            visited[kx + ky * grid.height] = true;
+
+            while let Some((tile, d)) = queue.pop_front() {
+                for ((x, y), tile) in grid.neighbors(tile) {
+                    if visited[x + y * grid.height] {
+                        continue;
+                    }
+
+                    visited[x + y * grid.height] = true;
+                    queue.push_back(((x, y), d + 1));
+
+                    if let Tile::Key(n) = tile {
+                        keys_distances[k][n as usize] = d + 1;
+                    }
+                }
+            }
+        }
+
+        // Resolve key dependencies : remove gates altogether by expanding the keys_requirements into a
+        // list of keys that all need to be collected before.
+        let mut key_dependencies = vec![None; grid.num_keys];
+
+        // Ugly recursive auxiliary function
+        fn get_key_dependencies(
+            memo: &mut [Option<BitSet>],
+            key: u8,
+            kreqs: &[Option<u8>],
+            greqs: &[Option<u8>],
+            cap: usize,
+        ) -> BitSet {
+            if let Some(set) = memo[key as usize].as_ref() {
+                return set.clone();
+            }
+
+            let mut dependencies = BitSet::empty();
+            let mut gate = kreqs[key as usize];
+            while let Some(g) = gate {
+                dependencies.insert(g as usize);
+                dependencies.concat(get_key_dependencies(memo, g, kreqs, greqs, cap));
+                gate = greqs[g as usize];
+            }
+
+            memo[key as usize] = Some(dependencies.clone());
+            dependencies
+        }
+
+        for k in 0..grid.num_keys {
+            let _ = get_key_dependencies(
+                &mut key_dependencies,
+                k as u8,
+                &keys_requirements,
+                &gate_requirements,
+                grid.num_keys,
+            );
+        }
+
+        let dependencies = key_dependencies
+            .into_iter()
+            .map(|o| o.unwrap_or(BitSet::empty()))
+            .collect_vec();
+
+        let mut robot_keys = [BitSet::empty(); R];
+
+        for k in 0..grid.num_keys {
+            robot_keys[keys_robot[k]].insert(k);
+        }
 
         Self {
-            pos: new_pos,
-            grabbed_keys,
-            steps: self.steps + 1,
+            distances: keys_distances,
+            count: grid.num_keys,
+            dependencies,
+            robot_keys,
         }
     }
-}
 
-fn explore(grid: &Grid) -> Result<usize> {
-    let mut visited = FxHashSet::default();
-    let mut queue = VecDeque::new();
+    fn explore(&self) -> usize {
+        // Dijkstra on the simplified graph
+        let mut queue = BinaryHeap::new();
+        let mut dist = FxHashMap::default();
+        let start = State {
+            dist: 0,
+            poses: [self.count; R],
+            keys: BitSet::empty(),
+        };
+        dist.insert(start, 0);
+        queue.push(start);
 
-    let start_state = State::new(grid);
-    visited.insert(start_state.clone());
-    queue.push_back(start_state);
+        let goal = BitSet::from_range(0..self.count);
 
-    while let Some(state) = queue.pop_front() {
-        for (new_pos, tile) in grid.neighbors(state.pos, &state.grabbed_keys) {
-            let new_state = state.step(new_pos, tile);
+        while let Some(state) = queue.pop() {
+            if state.keys.contains_set(&goal) {
+                return state.dist;
+            }
 
-            if visited.contains(&new_state) {
+            if state.dist > dist[&state] {
                 continue;
             }
 
-            if new_state.grabbed_keys.is_full() {
-                return Ok(new_state.steps);
+            for robot in 0..R {
+                for n in self.robot_keys[robot].difference(&state.keys).into_iter().filter(|&k| {
+                    state.keys.contains_set(&self.dependencies[k])
+                }) {
+                    let weight = self.distances[state.poses[robot]][n];
+                    let n_state = {
+                        let mut poses = state.poses;
+                        poses[robot] = n;
+                        let mut keys = state.keys;
+                        if n != self.count {
+                            keys.insert(n);
+                        }
+
+                        State {
+                            keys,
+                            poses,
+                            dist: dist[&state] + weight,
+                        }
+                    };
+
+                    if n_state.dist < dist.get(&n_state).copied().unwrap_or(usize::MAX) {
+                        queue.push(n_state);
+                        dist.insert(n_state, n_state.dist);
+                    }
+                }
             }
-
-            visited.insert(new_state.clone());
-            queue.push_back(new_state);
         }
-    }
 
-    Err(anyhow!("Couldn't grab all the keys"))
+        panic!("Couldn't find shortest path");
+    }
 }
 
 pub async fn day18(input: String) -> Result<(String, String)> {
     let grid: Grid = input.parse()?;
+    let split_grid = grid.transform();
 
-    let part1 = explore(&grid)?;
-    let part2 = 0;
+    let (graph, split_graph) = (
+        KeyGraph::new(&grid, [grid.entrance]),
+        KeyGraph::new(&split_grid, split_grid.split_entrances()),
+    );
+
+    let part1 = graph.explore();
+    let part2 = split_graph.explore();
 
     Ok((part1.to_string(), part2.to_string()))
 }
-
--- 2019/src/challenges/day19.rs
+++ 2019/src/challenges/day19.rs
@@ -1,5 +1,143 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 
-pub async fn day19(_input: String) -> Result<(String, String)> {
-    Err(anyhow!("Unimplemented"))
+use crate::intcode::Intcode;
+
+struct Beam {
+    program: Intcode<191>,
+    is_down: bool,
 }
+
+impl Beam {
+    fn new(program: Intcode<191>) -> Self {
+        let mut res = Self {
+            program,
+            is_down: false,
+        };
+        res.is_down = res.find_beam_orientation();
+        res
+    }
+    fn find_beam_orientation(&self) -> bool {
+        for d in 1.. {
+            for x in 0..=d {
+                if self.contains((x, d - x)) {
+                    return 2 * x <= d;
+                }
+            }
+        }
+
+        unreachable!()
+    }
+    fn contains(&self, (x, y): (i64, i64)) -> bool {
+        self.program
+            .clone()
+            .run_until_output(&[x, y])
+            .expect("Error when running tractor beam program")
+            .expect("Tractor beam program returned with no output")
+            == 1
+    }
+    fn step((x, y): (i64, i64), down: bool) -> (i64, i64) {
+        if down { (x, y + 1) } else { (x + 1, y) }
+    }
+    fn unstep((x, y): (i64, i64), down: bool) -> (i64, i64) {
+        if down { (x, y - 1) } else { (x - 1, y) }
+    }
+}
+
+struct BeamBoundIter<'a> {
+    beam: &'a Beam,
+    pos: (i64, i64),
+    is_lost: bool,
+    is_top: bool,
+}
+
+impl<'a> BeamBoundIter<'a> {
+    fn new(beam: &'a Beam, top: bool) -> Self {
+        Self {
+            beam,
+            pos: (0, 0),
+            is_lost: false,
+            is_top: top,
+        }
+    }
+    fn step(&mut self, reverse: bool) {
+        self.pos = Beam::step(self.pos, self.beam.is_down != reverse)
+    }
+    fn is_on(&self) -> bool {
+        self.beam.contains(self.pos)
+    }
+}
+
+impl<'a> Iterator for BeamBoundIter<'a> {
+    type Item = (i64, i64);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_top == !self.beam.is_down {
+            self.step(false);
+            if self.is_on() {
+                self.is_lost = false;
+            } else if !self.is_lost {
+                self.step(true);
+                self.is_lost = true;
+            }
+            Some(self.pos)
+        } else {
+            self.step(!self.is_lost);
+            if self.is_on() {
+                self.is_lost = false;
+                Some(self.pos)
+            } else {
+                if !self.is_lost {
+                    self.step(false);
+                    if self.is_on() {
+                        return Some(self.pos);
+                    }
+                    self.is_lost = true;
+                }
+                Some(Beam::unstep(self.pos, !self.beam.is_down))
+            }
+        }
+    }
+}
+
+fn part1(beam: &Beam) -> i64 {
+    let mut total = 0i64;
+    for y in 0..50 {
+        for x in 0..50 {
+            let is_in = beam.contains((x, y));
+            total += is_in as i64;
+        }
+    }
+
+    total
+}
+
+fn part2(beam: &Beam) -> i64 {
+    let box_size = 100;
+
+    let ((tx, ty), (bx, by)) = BeamBoundIter::new(beam, true)
+        .skip(if beam.is_down { 0 } else { box_size - 1 })
+        .zip(BeamBoundIter::new(beam, false).skip(if beam.is_down { box_size - 1 } else { 0 }))
+        .find(|&((tx, ty), (bx, by))| {
+            if beam.is_down {
+                tx - bx + 1 >= box_size as i64
+            } else {
+                by - ty + 1 >= box_size as i64
+            }
+        })
+        .unwrap();
+
+    if beam.is_down {
+        bx * 10000 + ty
+    } else {
+        tx * 10000 + by
+    }
+}
+
+pub async fn day19(input: String) -> Result<(String, String)> {
+    let beam = Beam::new(input.parse()?);
+
+    let part1 = part1(&beam);
+    let part2 = part2(&beam);
+
+    Ok((part1.to_string(), part2.to_string()))
+}
+
