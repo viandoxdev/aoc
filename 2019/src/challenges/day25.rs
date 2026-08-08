0326456ca65f1b9af9056b9f3b55358ba83376d9
viandoxdev <titouangoulois29@gmail.com>
2026-08-08 19:50:35 +0200

    Speed up and tidying

--- 2019/src/challenges/day25.rs
+++ 2019/src/challenges/day25.rs
@@ -9,9 +9,9 @@ use itertools::Itertools;
 
 use crate::intcode::{Intcode, ProgramState};
 
-const OUTPUT_LEN_CAP: usize = 5000;
+const OUTPUT_LEN_CAP: usize = 1000;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     West = 0,
     North = 1,
@@ -20,6 +20,13 @@ enum Direction {
 }
 
 impl Direction {
+    const ALL: [Direction; 4] = [
+        Direction::West,
+        Direction::North,
+        Direction::East,
+        Direction::South,
+    ];
+
     fn opposite(self) -> Self {
         match self {
             Direction::West => Direction::East,
@@ -43,6 +50,19 @@ impl TryFrom<usize> for Direction {
     }
 }
 
+impl TryFrom<&str> for Direction {
+    type Error = anyhow::Error;
+    fn try_from(value: &str) -> Result<Self> {
+        match value {
+            "west" => Ok(Direction::West),
+            "north" => Ok(Direction::North),
+            "east" => Ok(Direction::East),
+            "south" => Ok(Direction::South),
+            d => Err(anyhow!("Invalid direction '{d}'")),
+        }
+    }
+}
+
 impl Display for Direction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -57,6 +77,7 @@ impl Display for Direction {
 #[derive(Debug, Clone)]
 enum Command {
     Take(String),
+    Drop(String),
     Move(Direction),
 }
 
@@ -64,14 +85,35 @@ impl Display for Command {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Command::Take(s) => write!(f, "take {s}"),
+            Command::Drop(s) => write!(f, "drop {s}"),
             Command::Move(s) => write!(f, "{s}"),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Room {
     neighbors: [Option<String>; 4],
+}
+
+impl Room {
+    fn neighbor(&self, dir: Direction) -> Option<&str> {
+        self.neighbors[dir as usize].as_deref()
+    }
+
+    fn exits(&self) -> impl Iterator<Item = (Direction, &str)> {
+        Direction::ALL
+            .into_iter()
+            .filter_map(|d| Some((d, self.neighbor(d)?)))
+    }
+
+    fn exit_toward(&self, dest: &str) -> Option<Direction> {
+        self.exits().find(|(_, n)| *n == dest).map(|(d, _)| d)
+    }
+
+    fn any_exit(&self) -> Option<Direction> {
+        self.exits().next().map(|(d, _)| d)
+    }
 }
 
 #[derive(Debug)]
@@ -89,10 +131,11 @@ struct Droid {
     program: Intcode<251>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum CommandFeedback {
     Halted,
     ItemPickedUp,
+    ItemDropped,
     Other,
     RoomInformation(CurrentRoomInformation),
     Finished(String),
@@ -107,7 +150,7 @@ impl CommandFeedback {
     }
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default)]
 struct CurrentRoomInformation {
     name: String,
     items: Vec<String>,
@@ -117,63 +160,89 @@ struct CurrentRoomInformation {
 }
 
 impl Droid {
+    fn take(&mut self, item: &str) -> Result<CommandFeedback> {
+        self.step(Some(Command::Take(item.to_string())))
+    }
+
+    fn drop_item(&mut self, item: &str) -> Result<CommandFeedback> {
+        self.step(Some(Command::Drop(item.to_string())))
+    }
+
+    fn go(&mut self, dir: Direction) -> Result<CommandFeedback> {
+        self.step(Some(Command::Move(dir)))
+    }
+
+    /// Pumps the Intcode VM until it awaits more input or finishes.
+    /// Returns None if the output cap is exceeded (signals an infinite loop).
+    fn run_to_input(&mut self, mut input: &[i64]) -> Result<Option<(bool, Vec<i64>)>> {
+        let mut output = Vec::new();
+        let finished = loop {
+            if output.len() > OUTPUT_LEN_CAP {
+                return Ok(None);
+            }
+            match self.program.step(input.first().copied())? {
+                ProgramState::Running => {}
+                ProgramState::Finished => break true,
+                ProgramState::AwaitingInput => break false,
+                ProgramState::ConsumedInput => input = &input[1..],
+                ProgramState::PendingOutput(out) => output.push(out),
+            }
+        };
+        Ok(Some((finished, output)))
+    }
+
     fn parse_program_output(output: String) -> Result<CommandFeedback> {
         let mut inf = CurrentRoomInformation::default();
         let mut res = CommandFeedback::Other;
 
-        #[derive(Debug, Clone, Copy)]
+        #[derive(Clone, Copy)]
         enum State {
             None,
             Exits,
             Items,
         }
-
         let mut state = State::None;
 
         for line in output.lines() {
             if line.starts_with("== ") {
-                if inf.name == "" {
+                if inf.name.is_empty() {
                     inf.name = line[3..(line.len() - 3)].to_string();
                 } else {
                     inf.kicked_back = true;
                 }
-
                 state = State::None;
             } else if line == "Doors here lead:" {
                 state = State::Exits;
             } else if line == "Items here:" {
                 state = State::Items;
             } else if line.contains("Alert!") {
-                if line.contains("lighter") {
-                    inf.pressure_diag = Some(Ordering::Less)
+                inf.pressure_diag = Some(if line.contains("lighter") {
+                    Ordering::Less
                 } else {
-                    inf.pressure_diag = Some(Ordering::Greater)
-                }
+                    Ordering::Greater
+                });
             } else if line.contains("get in by typing") {
                 res = CommandFeedback::Finished(
                     line.chars().filter(|c| c.is_ascii_digit()).collect(),
                 );
-                inf.name = "".to_string();
+                inf.name = String::new();
             } else if line.starts_with("- ") {
                 match state {
                     State::None => return Err(anyhow!("List item outside of item or exits scope")),
-                    State::Exits => match line {
-                        "- west" => inf.exits[Direction::West as usize] = true,
-                        "- east" => inf.exits[Direction::East as usize] = true,
-                        "- north" => inf.exits[Direction::North as usize] = true,
-                        "- south" => inf.exits[Direction::South as usize] = true,
-                        _ => return Err(anyhow!("Unrecognized exit door '{line}'")),
-                    },
-                    State::Items => {
-                        inf.items.push(line[2..].to_string());
+                    State::Exits => {
+                        let dir = Direction::try_from(&line[2..])?;
+                        inf.exits[dir as usize] = true;
                     }
+                    State::Items => inf.items.push(line[2..].to_string()),
                 }
             } else if line.starts_with("You take") {
-                res = CommandFeedback::ItemPickedUp
+                res = CommandFeedback::ItemPickedUp;
+            } else if line.starts_with("You drop") {
+                res = CommandFeedback::ItemDropped;
             }
         }
 
-        if inf.name != "" {
+        if !inf.name.is_empty() {
             Ok(CommandFeedback::RoomInformation(inf))
         } else {
             Ok(res)
@@ -181,7 +250,7 @@ impl Droid {
     }
 
     fn step(&mut self, command: Option<Command>) -> Result<CommandFeedback> {
-        let input = match command {
+        let input: Vec<i64> = match command {
             Some(c) => c
                 .to_string()
                 .bytes()
@@ -191,30 +260,16 @@ impl Droid {
             None => Vec::new(),
         };
 
-        let mut input = &input[..];
-        let mut output = Vec::new();
-
-        let finished = loop {
-            if output.len() > OUTPUT_LEN_CAP {
-                // Ran into infinite loop
-                return Ok(CommandFeedback::Halted);
-            }
-
-            match self.program.step(input.first().copied())? {
-                ProgramState::Running => {}
-                ProgramState::Finished => break true,
-                ProgramState::AwaitingInput => break false,
-                ProgramState::ConsumedInput => input = &input[1..],
-                ProgramState::PendingOutput(out) => output.push(out),
-            };
+        let Some((finished, raw)) = self.run_to_input(&input)? else {
+            return Ok(CommandFeedback::Halted);
         };
 
-        let output: String = output
+        let output: String = raw
             .iter()
             .filter_map(|&c| char::from_u32(c as u32))
             .collect();
-
         let res = Self::parse_program_output(output)?;
+
         if finished && !matches!(res, CommandFeedback::Finished(_)) {
             return Ok(CommandFeedback::Halted);
         }
@@ -223,7 +278,6 @@ impl Droid {
     }
 }
 
-#[derive(Debug, Clone)]
 struct OnesIterator(u16);
 
 impl Iterator for OnesIterator {
@@ -242,11 +296,10 @@ impl Iterator for OnesIterator {
 impl Spaceship {
     pub fn explore(mut droid: Droid) -> Result<Self> {
         let mut queue = VecDeque::new();
-
-        let mut rooms = HashMap::new();
+        let mut rooms: HashMap<String, Room> = HashMap::new();
         let mut goal = String::new();
         let mut gateway = String::new();
-        let mut item_start_room = HashMap::new();
+        let mut item_start_room: HashMap<String, String> = HashMap::new();
 
         let start = droid
             .step(None)?
@@ -257,27 +310,20 @@ impl Spaceship {
             item_start_room.insert(item.clone(), start.name.clone());
         }
 
-        rooms.insert(
-            start.name.clone(),
-            Room {
-                neighbors: [None, None, None, None],
-            },
-        );
+        rooms.insert(start.name.clone(), Room::default());
         queue.push_back((start.name.clone(), start.exits, droid.clone()));
 
         let start_room = start.name;
 
         while let Some((name, exits, droid)) = queue.pop_front() {
-            for (i, _) in exits.into_iter().enumerate().filter(|(_, b)| *b) {
-                let d = Direction::try_from(i)?;
-
+            for d in Direction::ALL.into_iter().filter(|d| exits[*d as usize]) {
                 let mut next_droid = droid.clone();
                 let next_pos = next_droid
-                    .step(Some(Command::Move(d)))?
+                    .go(d)?
                     .room_information()
                     .context("Move failed")?;
 
-                rooms.get_mut(&name).unwrap().neighbors[i] = Some(next_pos.name.clone());
+                rooms.get_mut(&name).unwrap().neighbors[d as usize] = Some(next_pos.name.clone());
 
                 if next_pos.kicked_back {
                     goal = next_pos.name.clone();
@@ -288,15 +334,8 @@ impl Spaceship {
                     for item in &next_pos.items {
                         item_start_room.insert(item.clone(), next_pos.name.clone());
                     }
-                    rooms.insert(
-                        next_pos.name.clone(),
-                        Room {
-                            neighbors: [None, None, None, None],
-                        },
-                    );
-
+                    rooms.insert(next_pos.name.clone(), Room::default());
                     if !next_pos.kicked_back {
-                        // Don't explore further if we can't
                         queue.push_back((next_pos.name, next_pos.exits, next_droid));
                     }
                 }
@@ -313,142 +352,133 @@ impl Spaceship {
         })
     }
 
-    fn path_to(&self, droid: &mut Droid, from: String, mut to: String) -> Result<()> {
-        let mut prev: HashMap<&String, Option<Direction>> = HashMap::new();
-        let mut queue = VecDeque::new();
+    fn find_path(&self, from: String, mut to: String) -> Vec<Direction> {
+        let mut prev: HashMap<&str, Option<Direction>> = HashMap::new();
+        let mut queue: VecDeque<&str> = VecDeque::new();
 
         prev.insert(&from, None);
         queue.push_back(&from);
 
         while let Some(pos) = queue.pop_front() {
-            if pos == &to {
+            if pos == to {
                 break;
             }
 
-            for (i, r) in self.rooms[pos]
-                .neighbors
-                .iter()
-                .enumerate()
-                .filter_map(|(i, r)| Some((i, r.as_ref()?)))
-            {
-                let dir = Direction::try_from(i).unwrap();
-
-                if !prev.contains_key(r) {
-                    prev.insert(r, Some(dir));
-                    queue.push_back(r);
+            for (dir, neighbor) in self.rooms[pos].exits() {
+                if !prev.contains_key(neighbor) {
+                    prev.insert(neighbor, Some(dir));
+                    queue.push_back(neighbor);
                 }
             }
         }
 
         let mut path = Vec::new();
 
-        while let Some(&Some(dir)) = prev.get(&to) {
+        while let Some(&Some(dir)) = prev.get(to.as_str()) {
             path.push(dir);
-            to = self.rooms[&to].neighbors[dir.opposite() as usize]
-                .as_ref()
-                .unwrap()
-                .clone();
+            to = self.rooms[&to].neighbor(dir.opposite()).unwrap().to_owned();
         }
 
         path.reverse();
+        path
+    }
 
-        for dir in path {
-            droid.step(Some(Command::Move(dir)))?;
+    fn path_to(&self, droid: &mut Droid, from: &str, to: &str) -> Result<()> {
+        for dir in self.find_path(from.to_string(), to.to_string()) {
+            droid.go(dir)?;
         }
 
         Ok(())
     }
 
-    fn identify_trap_items(&self) -> Result<Vec<String>> {
-        let mut trapped = Vec::new();
+    fn is_trap_item(&self, item: &str) -> Result<bool> {
+        let room = &self.item_start_room[item];
 
-        for (item, room) in &self.item_start_room {
-            let mut droid = self.droid_template.clone();
-            self.path_to(&mut droid, self.start.clone(), room.clone())?;
+        let mut droid = self.droid_template.clone();
 
-            if !matches!(
-                droid.step(Some(Command::Take(item.clone())))?,
-                CommandFeedback::ItemPickedUp
-            ) {
-                trapped.push(item.clone());
-                continue;
-            };
+        self.path_to(&mut droid, &self.start, room)?;
 
-            let exit = Direction::try_from(
-                self.rooms[room]
-                    .neighbors
-                    .iter()
-                    .position(|o| o.is_some())
-                    .context("Room doesn't have any exit")?,
-            )
-            .unwrap();
-
-            if let Some(state) = droid.step(Some(Command::Move(exit)))?.room_information() {
-                if &state.name != room {
-                    continue;
-                }
-            }
-
-            trapped.push(item.clone());
+        if !matches!(droid.take(item)?, CommandFeedback::ItemPickedUp) {
+            return Ok(true);
         }
 
-        Ok(trapped)
+        let exit = self.rooms[room]
+            .any_exit()
+            .context("Room doesn't have any exit")?;
+        let safe = droid
+            .go(exit)?
+            .room_information()
+            .is_some_and(|s| &s.name != room);
+        Ok(!safe)
+    }
+
+    fn identify_trap_items(&self) -> Result<Vec<String>> {
+        self.item_start_room
+            .keys()
+            .filter_map(|item| match self.is_trap_item(item) {
+                Ok(true) => Some(Ok(item.clone())),
+                Ok(false) => None,
+                Err(e) => Some(Err(e)),
+            })
+            .collect()
+    }
+
+    /// Clones the template droid, collects every safe item, and positions it at the gateway.
+    /// Returns the droid and the direction from the gateway into the goal room.
+    fn load_droid_at_gateway(&self, safe_items: &[&String]) -> Result<(Droid, Direction)> {
+        let mut droid = self.droid_template.clone();
+        let mut pos = self.start.as_str();
+
+        for item in safe_items {
+            let room = &self.item_start_room[*item];
+            self.path_to(&mut droid, pos, room)?;
+            pos = room;
+            droid.take(item)?;
+        }
+
+        self.path_to(&mut droid, pos, &self.gateway)?;
+        let dir = self.rooms[&self.gateway].exit_toward(&self.goal).unwrap();
+        Ok((droid, dir))
     }
 
     fn solve(&self) -> Result<String> {
         let trapped = self.identify_trap_items()?;
-        let safe_items = self
+        let safe_items: Vec<&String> = self
             .item_start_room
             .keys()
             .filter(|k| !trapped.contains(k))
-            .cloned()
-            .collect_vec();
+            .collect();
 
         let count = safe_items.len();
+        let (mut droid, dir) = self.load_droid_at_gateway(&safe_items)?;
 
-        let mut heavy: Vec<u16> = Vec::new();
-        let mut light: Vec<u16> = Vec::new();
-        let mut subsets = (0u16..(1u16 << count)).collect_vec();
+        let mut subsets: Vec<u16> = (0u16..(1u16 << count)).collect();
         subsets.sort_by_key(|s| s.count_ones());
+
+        let full = (1u16 << count) - 1;
+        let mut current_set = full;
+        let mut heavy: Vec<u16> = Vec::new();
 
         for set in subsets {
             if heavy.iter().any(|&s| set & s == s) {
                 continue;
             }
-            if light.iter().any(|&s| set & s == set) {
-                continue;
+
+            for i in OnesIterator(current_set & !set) {
+                droid.drop_item(safe_items[i])?;
             }
-
-            let selected = OnesIterator(set).map(|i| &safe_items[i]);
-
-            let mut droid = self.droid_template.clone();
-
-            let mut pos = self.start.clone();
-            for item in selected {
-                let room = &self.item_start_room[item];
-                self.path_to(&mut droid, pos, room.clone())?;
-                pos = room.clone();
-                droid.step(Some(Command::Take(item.clone())))?;
+            for i in OnesIterator(set & !current_set) {
+                droid.take(safe_items[i])?;
             }
-            self.path_to(&mut droid, pos, self.gateway.clone())?;
+            current_set = set;
 
-            let dir = Direction::try_from(
-                self.rooms[&self.gateway]
-                    .neighbors
-                    .iter()
-                    .position(|r| r.as_ref() == Some(&self.goal))
-                    .unwrap(),
-            )
-            .unwrap();
-
-            match droid.step(Some(Command::Move(dir)))? {
+            match droid.go(dir)? {
                 CommandFeedback::Finished(pass) => return Ok(pass),
                 CommandFeedback::RoomInformation(CurrentRoomInformation {
                     pressure_diag: Some(diag),
                     ..
                 }) => match diag {
                     Ordering::Less => heavy.push(set),
-                    Ordering::Greater => light.push(set),
                     _ => {}
                 },
                 _ => {}
